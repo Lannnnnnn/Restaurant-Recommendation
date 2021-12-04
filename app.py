24704fdebfe55e0aa788e2f87bd7472191b55117
Lannnnnnn <shenghanliu35@gmail.com>
2021-12-03 19:49:53 -0800

    finish everything

--- app.py
+++ app.py
@@ -10,13 +10,14 @@ app.config['UPLOAD_PATH'] = 'static'
 app.secret_key = os.urandom(24)
 app.static_folder = 'static'
 
+# link to the firebase
 url = 'https://project-3e24f-default-rtdb.firebaseio.com/'
 
 @app.route('/')
 def index():
     return render_template('homepage.html')
 
-#Takes in the query, location, and food/restaurant button
+# Read and Upload the user input data to the Firebase
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
     if request.method == "POST":
@@ -34,7 +35,9 @@ def upload():
                 session['b_name'] = filename
             else:
                 abort(400)
+        # clean the previous Firebase data storage record
         util.clear_firebase_cache()
+        # data cleaning and store the data to Firebase
         util.json_to_csv(path['review_path'], path['business_path'])
 
         return render_template('upload.html')
@@ -42,13 +45,18 @@ def upload():
     else:
         return redirect(url_for('index'))
 
-
+# retreive the metadata information for the user input file and dataframe first 5 rows based on the city user chosen
 @app.route("/information", methods = ['POST', "GET"])
 def information():
     if request.method == "POST":
+        # get the city name the user chosen
         city = request.form['city']
+        # retrieve the data from the firebase
         r_if = requests.get(url = url + 'review_info.json').json()
         b_if = requests.get(url = url + 'business_info.json').json()
+        business = requests.get(url = url + 'business.json?orderBy="city"&equalTo="'+str(city)+'"').json()
+        review = requests.get(url = url + 'review.json').json()
+        # store the metadata
         b_res = [b_if['file name'],
                 b_if['file size'],
                 b_if['number of attributes'],
@@ -60,42 +68,45 @@ def information():
                  r_if['number of attributes'],
                  r_if['attributes name'],
                  r_if['number of rows']]
-            
-        business = requests.get(url = url + 'business.json?orderBy="city"&equalTo="'+str(city)+'"').json()
-        review = requests.get(url = url + 'review.json').json()
-
+        # convert the retrieved data to dataframe and filter out the business infomation in the chosen city
         city_business = pd.DataFrame.from_dict(business, orient="index")
         city_rest = city_business[(city_business.categories.str.contains('Restaurants')
         )| (city_business.categories.str.contains('Food'))].reset_index(drop = True)
         review_df = pd.json_normalize(review)
         city_review = review_df[review_df['business_id'].isin(city_rest['business_id'].tolist())].reset_index(drop = True)
-
+        # group the text reivew by restaurant and upload the grouped information to the firebase  
         grouped_rest = util.preprocess_group_restaurant_review(city_review, city_rest)
         requests.put(url = url + 'grouped_df.json', data = grouped_rest.to_json(orient = 'records'))
         return render_template('information.html', r = r_res, r_df = city_review, b = b_res, b_df = city_rest)
     else:
         return render_template('upload.html')
 
+# jump to the recommendation function
 @app.route("/recommendation", methods = ['POST', 'GET'])
 def recommendation():
     if request.method == 'POST':
         return redirect(url_for('update'))
     else:
+
         return redirect(url_for('information'))
 
+# generate the N recommendation restaurants based on the user choice
 @app.route("/update", methods = ['POST', 'GET'])
 def update():
     if request.method == 'POST':
+        # get the restaurant name and number of recommendations the user chosen
         text = request.form.get('querybox')
         rank = int(request.form.get('rank'))
 
+        # retrieve the grouped dataframe from the firebase
         grouped_df = pd.json_normalize(requests.get(url = url + 'grouped_df.json').json())
+        # run the tfidf algorithm on the data
         tfidf_df = util.perform_tfidf(grouped_df)
         feature_df = util.generate_n_dataframe(tfidf_df, rank)
 
         with_business_id = grouped_df[['business_id','stars','name']].merge(feature_df)
         all_info = grouped_df[['business_id','categories','review_count','address']].merge(with_business_id).drop_duplicates()
-
+        # generate N top recommendations based on the tfidf results
         output = util.output_recommendation(all_info, text, rank)
         return jsonify({'result':'success', 'data': output})
     else:
